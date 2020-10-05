ff37eaa1f9cdd2897c2fced9fabd5639551e14db
29781 <2978183194@qq.com>
2020-10-05 22:43:54 +0800

    增加登录隐藏符号

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -3,10 +3,10 @@
     <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
       <h2 class="login-title">后台管理系统</h2>
       <el-form-item label="用户名">
-        <el-input v-model="formdata.name" placeholder="请输入用户名"></el-input>
+        <el-input v-model="formdata.name" icon="el-icon-user-solid" placeholder="请输入用户名"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formdata.password" placeholder="请输入密码"></el-input>
+        <el-input v-model="formdata.password" placeholder="请输入密码" show-password></el-input>
       </el-form-item>
       <el-button type="primary" class="login-btn">登录</el-button>
     </el-form>
