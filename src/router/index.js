b8b1d392d1864f5a0fe0df0ac28fb2ef2c70a23b
29781 <2978183194@qq.com>
2020-10-05 21:01:54 +0800

    新建登录分支新建登录主件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login/login.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/'
-    }
-  ]
+  routes: [{
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
