75373e863c5b4f58da267b895d88f334e3a39af1
29781 <2978183194@qq.com>
2020-10-06 23:01:38 +0800

    完成登录页面编写

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="login-wrap">
-    <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
-      <h2 class="login-title">后台管理系统</h2>
+    <el-form  ref="LoginFormRef" :rules="loginFormRules" label-position="top" label-width="80px" :model="formdata" class="login-form">
+      <h2 class="login-title">体育后台管理系统</h2>
       <el-form-item label="用户名">
-        <el-input v-model="formdata.username" placeholder="请输入用户名"></el-input>
+        <el-input v-model="formdata.username" placeholder="请输入用户名" ></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password" placeholder="请输入密码" show-password></el-input>
@@ -20,13 +20,51 @@ export default {
       formdata: {
         username: '',
         password: ''
+      },
+      // 表单验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '登录名长度在 3 到 10 个字符',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: '密码长度在 6 到 15 个字符',
+            trigger: 'blur'
+          }
+        ]
       }
     }
   },
   methods: {
     handleLogin () {
-      this.$http.post('/login', this.formdata).then(res => {
+      // 点击登录的时候先调用validate方法验证表单内容是否有误
+      this.$refs.LoginFormRef.validate(async valid => {
+        console.log(this.loginFormRules)
+        // 如果valid参数为true则验证通过
+        if (!valid) {
+          return
+        }
+        // 发送请求进行登录
+        const { data: res } = await this.$http.post('/login', this.formdata)
+        //   console.log(res);
+        if (res.code !== 200) {
+          return this.$message.error('登录失败:' + res.message) // console.log("登录失败:"+res.meta.msg)
+        }
+        this.$message.success('登录成功')
         console.log(res)
+        // 保存token
+        window.sessionStorage.setItem('token', res.data.token)
+        // 导航至/home
+        this.$router.push('/home')
       })
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login.vue'
+import Home from '@/components/home/home.vue'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    name: 'login',
-    path: '/login',
-    component: Login
-  }]
+  routes: [
+    {name: 'login', path: '/login', component: Login},
+    {name: 'home', path: '/home', component: Home}
+  ]
 })
